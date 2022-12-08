23ad1b3fa0a15892cb864b480cf6ae6d697ea1bb
Marc <marc_charpentier@hotmail.fr>
2022-12-08 15:41:08 +0100

    displaying transport images

--- src/components/TraficContent/TraficContent.jsx
+++ src/components/TraficContent/TraficContent.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import useAxios from "../../hooks/useAxios";
+import getImageByKey from "../../utils/getImageByKey";
 
 export default function TraficContent() {
   const [data, setData] = useState([]);
@@ -27,6 +28,29 @@ export default function TraficContent() {
   return (
     <div className="Trafic">
       <p>Voici les infos Trafic</p>
+      <div className="Trafic-Content">
+        <div className="Trafic-Metros flex">
+          {data?.metros?.map((metro) => (
+            <div className="pr-4 w-16 h-16" key={metro.line}>
+              <img src={getImageByKey(`metro${metro.line}`)} />
+            </div>
+          ))}
+        </div>
+        <div className="Trafic-Rers flex">
+          {data?.rers?.map((rer) => (
+            <div className="pr-4 w-16 h-16" key={rer.line}>
+              <img src={getImageByKey(`rer${rer.line}`)} />
+            </div>
+          ))}
+        </div>
+        <div className="Trafic-Tramways flex">
+          {data?.tramways?.map((tramway) => (
+            <div className="pr-4 w-16 h-16" key={tramway.line}>
+              <img src={getImageByKey(`tramway${tramway.line}`)} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/getImageByKey.js
@@ -0,0 +1,69 @@
+import metro1 from "../assets/M1genRVB.svg";
+import metro2 from "../assets/M2genRVB.svg";
+import metro3 from "../assets/M3genRVB.svg";
+import metro3B from "../assets/M3bisgenRVB.svg";
+import metro4 from "../assets/M4genRVB.svg";
+import metro5 from "../assets/M5genRVB.svg";
+import metro6 from "../assets/M6genRVB.svg";
+import metro7 from "../assets/M7genRVB.svg";
+import metro7B from "../assets/M7bisgenRVB.svg";
+import metro8 from "../assets/M8genRVB.svg";
+import metro9 from "../assets/M9genRVB.svg";
+import metro10 from "../assets/M10genRVB.svg";
+import metro11 from "../assets/M11genRVB.svg";
+import metro12 from "../assets/M12genRVB.svg";
+import metro13 from "../assets/M13genRVB.svg";
+import metro14 from "../assets/M14genRVB.svg";
+import rerA from "../assets/RERAgenRVB.svg";
+import rerB from "../assets/RERBgenRVB.svg";
+import rerC from "../assets/RERCgenRVB.svg";
+import rerD from "../assets/RERDgenRVB.svg";
+import rerE from "../assets/REREgenRVB.svg";
+import tramway1 from "../assets/T1genRVB.svg";
+import tramway2 from "../assets/T2genRVB.svg";
+import tramway3A from "../assets/T3A-genRVB.svg";
+import tramway3B from "../assets/T3B-genRVB.svg";
+import tramway4 from "../assets/T4genRVB.svg";
+import tramway5 from "../assets/T5genRVB.svg";
+import tramway6 from "../assets/T6genRVB.svg";
+import tramway7 from "../assets/T7genRVB.svg";
+import tramway8 from "../assets/T8genRVB.svg";
+
+const images = {
+  metro1,
+  metro2,
+  metro3,
+  metro3B,
+  metro4,
+  metro5,
+  metro6,
+  metro7,
+  metro7B,
+  metro8,
+  metro9,
+  metro10,
+  metro11,
+  metro12,
+  metro13,
+  metro14,
+  rerA,
+  rerB,
+  rerC,
+  rerD,
+  rerE,
+  tramway1,
+  tramway2,
+  tramway3A,
+  tramway3B,
+  tramway4,
+  tramway5,
+  tramway6,
+  tramway7,
+  tramway8,
+};
+
+function getImageByKey(key) {
+  return images[key];
+}
+
+export default getImageByKey;
